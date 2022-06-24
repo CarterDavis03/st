92a337ee158d1b0025f3cc9802b4617d029ac425
Carter Davis <carterdavis1928@gmail.com>
2022-06-24 18:40:09 +0100

    alpha patch

--- config.h
+++ config.h
@@ -94,6 +94,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 const char *colorname[] = {
 
@@ -472,3 +475,4 @@ static char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
+
